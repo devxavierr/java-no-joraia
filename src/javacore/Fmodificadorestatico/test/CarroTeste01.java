cd9a85c7e6e8d813cf42f6bfa5360ffc53d52c4c
devxavierr <devxavierr@gmail.com>
2025-02-11 20:54:54 -0300

    aula 63

--- src/javacore/Fmodificadorestatico/test/CarroTeste01.java
+++ src/javacore/Fmodificadorestatico/test/CarroTeste01.java
@@ -5,11 +5,12 @@ import javacore.Fmodificadorestatico.domain.Carro;
 public class CarroTeste01 {
     public static void main(String[] args) {
 
+        Carro.setVelocidadeLimite(180);
+
+
         Carro c1 = new Carro("BMW", 280);
         Carro c2 = new Carro("Mercedes", 275);
         Carro c3 = new Carro("Audi", 290);
-
-        Carro.velocidadeLimite = 180;
 
         c1.imprime();
         c2.imprime();
